d0dd189fcf26d6f0bba6873a179ea235bf82671d
yogendra singh <yogendra@i2k2.com>
2021-02-14 14:56:12 +0530

    initial commit

---
+++ Solution1/UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=UserMicroservice;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
